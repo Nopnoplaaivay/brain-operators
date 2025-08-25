98b25bfe5491ab686203988c75bd22cef8f1bf02
KhangVinh <vinhkhang378@gmail.com>
2025-08-25 22:26:18 +0700

    add trade_when

--- trade_when.py
+++ trade_when.py
@@ -14,10 +14,6 @@ if __name__ == "__main__":
     y = np.random.randn(T, N)
     y[np.random.random((T, N)) < 0.2] = np.nan 
     z = (np.random.random((T, N)) < 0.05).astype(int)  
-    
-    start = time.time()
-    result0 = trade_when(x, y, z)
-    time0 = time.time() - start
 
     df_x = pd.DataFrame(x)
     df_x.columns = [f"x_{i}" for i in range(N)]
@@ -26,11 +22,16 @@ if __name__ == "__main__":
     df_z = pd.DataFrame(z)
     df_z.columns = [f"z_{i}" for i in range(N)]
 
+    start = time.time()
+    result0 = trade_when(df_x, df_y, df_z)
+    time0 = time.time() - start
+
+
     df_results = pd.DataFrame(result0)
     df_results.columns = [f"tradewhen_{i}" for i in range(N)]
 
     df_final = pd.concat([df_x, df_y, df_z, df_results], axis=1)
     df_final.astype(np.float32)
-    df_final.to_csv("trade_when_output.csv", index=False)
+    # df_final.to_csv("trade_when_output.csv", index=False)
 
     print(f"Original trade_when time: {time0:.4f} seconds")
--- transformational/trade_when.py
+++ transformational/trade_when.py
@@ -2,8 +2,12 @@ import numpy as np
 import pandas as pd
 
 
-def trade_when(x, y, z):
-    out = np.where(z == True, 0, np.where(x == True, y, np.nan))
+def trade_when(x: pd.DataFrame, y: pd.DataFrame, z: pd.DataFrame) -> pd.DataFrame:
+    x_ = x.values
+    y_ = y.values
+    z_ = z.values
+
+    out = np.where(z_ == True, 0, np.where(x_ == True, y_, np.nan))
     df = pd.DataFrame(out)
     df.ffill(axis=0, inplace=True)
     df.fillna(0, inplace=True)
